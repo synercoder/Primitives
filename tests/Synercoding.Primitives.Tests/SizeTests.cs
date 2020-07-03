7839035f8f0acb4bc369ec78743b449db6ccb331
Gerard Gunnewijk <gerard.gunnewijk@live.nl>
2020-07-03 14:37:29 +0200

    Added few test cases

---
+++ tests/Synercoding.Primitives.Tests/SizeTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace Synercoding.Primitives.Tests
+{
+    public class SizeTests
+    {
+        [Fact]
+        public void EqualityOperator_InchAndMillimeters_NotEqual()
+        {
+            // Arrange
+            var mmSize = new Size(1, 1, Unit.Millimeters);
+            var inSize = new Size(1, 1, Unit.Inches);
+
+            // Act
+            var result = mmSize == inSize;
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void EqualityOperator_InchAnd254Millimeters_AreEqual()
+        {
+            // Arrange
+            var mmSize = new Size(25.4, 25.4, Unit.Millimeters);
+            var inSize = new Size(1, 1, Unit.Inches);
+
+            // Act
+            var result = mmSize == inSize;
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
